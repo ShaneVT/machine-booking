990e333b4aa628fade610062725b72bb73b46388
ShaneVT <shaneosullivan@vitaltrace.com.au>
2025-05-01 13:31:47 +0800

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -2,12 +2,11 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js';
 import { getFirestore }       from 'https://www.gstatic.com/firebasejs/9.22.1/firebase-firestore.js';
 
-// Replace these values with your Firebase project’s credentials:
 const firebaseConfig = {
   apiKey: "AIzaSyBuJv6jHOOnzvnHHoX9t_b3mTYeMK10tCM",
   authDomain: "machine-booking-3c611.firebaseapp.com",
-  projectId: "machine-booking-3c611",            // ← Must be your actual projectId
-  storageBucket: "machine-booking-3c611.appspot.com",
+  projectId: "machine-booking-3c611",
+  storageBucket: "machine-booking-3c611.firebasestorage.app",
   messagingSenderId: "417259615223",
   appId: "1:417259615223:web:8535395de07d7bce0db4f2"
 };
