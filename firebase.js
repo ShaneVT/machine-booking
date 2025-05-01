e62127769c9fbeae46e6700fe29bf062d465a34b
ShaneVT <shaneosullivan@vitaltrace.com.au>
2025-05-01 12:08:27 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,8 +4,8 @@ import { getFirestore }      from 'https://www.gstatic.com/firebasejs/9.22.1/fir
 const firebaseConfig = {
   apiKey: "AIzaSyBuJv6jHOOnzvnHHoX9t_b3mTYeMK10tCM",
   authDomain: "machine-booking-3c611.firebaseapp.com",
-  projectId: "machine-booking-3c611",
-  storageBucket: "machine-booking-3c611.firebasestorage.app",
+  projectId: "machine-booking-3c611",            // ← Must be your actual projectId
+  storageBucket: "machine-booking-3c611.appspot.com",
   messagingSenderId: "417259615223",
   appId: "1:417259615223:web:8535395de07d7bce0db4f2"
 };
