732c326531b86dd81201398bfa7e9a91affb2c5c
ShaneVT <shaneosullivan@vitaltrace.com.au>
2025-05-06 16:28:13 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 // Initialize Firebase
-const firebaseConfig = { /* YOUR CONFIG HERE */ };
+const firebaseConfig = {const firebaseConfig = {
+  apiKey: "AIzaSyBuJv6jHOOnzvnHHoX9t_b3mTYeMK10tCM",
+  authDomain: "machine-booking-3c611.firebaseapp.com",
+  projectId: "machine-booking-3c611",
+  storageBucket: "machine-booking-3c611.firebasestorage.app",
+  messagingSenderId: "417259615223",
+  appId: "1:417259615223:web:8535395de07d7bce0db4f2"
+};};
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 
