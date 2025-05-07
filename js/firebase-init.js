a7b2038949964f8e213328cc955c8cbfe25a5233
ShaneVT <shaneosullivan@vitaltrace.com.au>
2025-05-07 14:19:49 +0800

    Update firebase-init.js

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -1,20 +1,13 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBuJv6jHOOnzvnHHoX9t_b3mTYeMK10tCM",
+  authDomain: "machine-booking-3c611.firebaseapp.com",
+  projectId: "machine-booking-3c611",
+  storageBucket: "machine-booking-3c611.firebasestorage.app",
+  messagingSenderId: "417259615223",
+  appId: "1:417259615223:web:8535395de07d7bce0db4f2"
+};
+
 // Initialize Firebase
-if (!firebase.apps.length) {
-  const firebaseConfig = {
-    apiKey: "AIzaSyBuJv6jHOOnzvnHHoX9t_b3mTYeMK10tCM",
-    authDomain: "machine-booking-3c611.firebaseapp.com",
-    projectId: "machine-booking-3c611",
-    storageBucket: "machine-booking-3c611.appspot.com",
-    messagingSenderId: "417259615223",
-    appId: "1:417259615223:web:8535395de07d7bce0db4f2"
-  };
-  firebase.initializeApp(firebaseConfig);
-}
-
-// Export services
-const db = firebase.firestore();
-const auth = firebase.auth();
-
-// Make available globally for non-module scripts
-window.firebaseDb = db;
-window.firebaseAuth = auth;
+const app = initializeApp(firebaseConfig);
